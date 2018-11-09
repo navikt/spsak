82df60a2cb53c261f03539aaec83def08a9b1c64
Marius Glittum <Marius.Glittum@nav.no>
2018-11-09 09:26:07 +0100

    Vedtaksxml - retter compile error

--- vl-foreldrepenger/domenetjenester/vedtak-xml/src/main/java/no/nav/foreldrepenger/vedtak/xml/behandlingsresultat/beregningsresultat/beregningsgrunnlag/BeregningsgrunnlagXmlTjenesteEngangsstønad.java
+++ vl-foreldrepenger/domenetjenester/vedtak-xml/src/main/java/no/nav/foreldrepenger/vedtak/xml/behandlingsresultat/beregningsresultat/beregningsgrunnlag/BeregningsgrunnlagXmlTjenesteEngangsstønad.java
@@ -5,12 +5,12 @@ import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
-import no.nav.foreldrepenger.behandlingslager.behandling.repository.BeregningRepository;
-import no.nav.foreldrepenger.vedtak.xml.VedtakXmlUtil;
 import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.behandlingslager.behandling.beregning.Beregning;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BeregningRepository;
+import no.nav.foreldrepenger.vedtak.xml.VedtakXmlUtil;
 import no.nav.vedtak.felles.xml.vedtak.beregningsgrunnlag.es.v2.BeregningsgrunnlagEngangsstoenad;
 import no.nav.vedtak.felles.xml.vedtak.beregningsgrunnlag.es.v2.ObjectFactory;
 import no.nav.vedtak.felles.xml.vedtak.v2.Beregningsgrunnlag;
@@ -39,7 +39,7 @@ public class BeregningsgrunnlagXmlTjenesteEngangsstønad implements Beregningsgr
         Optional<Beregning> sisteBeregning = beregningRepository.getSisteBeregning(behandling.getId());
         if (sisteBeregning.isPresent()) {
             beregningsgrunnlag.setAntallBarn(VedtakXmlUtil.lagIntOpplysning((int) sisteBeregning.get().getAntallBarn()));
-            beregningsgrunnlag.setSats((int) sisteBeregning.get().getSatsVerdi());
+            beregningsgrunnlag.setSats(VedtakXmlUtil.lagLongOpplysning(sisteBeregning.get().getSatsVerdi()));
         }
         Beregningsgrunnlag beregningsgrunnlag1 = new Beregningsgrunnlag();
         beregningsgrunnlag1.getAny().add(beregningsgrunnlag);
