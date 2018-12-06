2f9fc81025b3fd459e0ed070ca9ae030b2c742ec
royag-nav <roy.andre.gundersen@nav.no>
2018-12-06 10:57:38 +0100

    fiks så ikke felles_behandlingsprosess_unit går i beina på fpsak_unit (må opprette DB + bruker + rettigheter manuelt)

--- felles/behandlingsprosess/migreringer/src/test/resources/db/migration/vl_dba/V1.0__Lokalt_skjemaoppsett.sql
+++ felles/behandlingsprosess/migreringer/src/test/resources/db/migration/vl_dba/V1.0__Lokalt_skjemaoppsett.sql
@@ -6,32 +6,39 @@ BEGIN
   SELECT count(*)
   INTO userexists
   FROM SYS.ALL_USERS
-  WHERE USERNAME = upper('${felles_behandlingsprosess_schema_unit}');
+  WHERE USERNAME = upper('DOLLAR{felles_behandlingsprosess_schema_unit}');
   IF (userexists = 0)
   THEN
-    EXECUTE IMMEDIATE ('CREATE USER ${felles_behandlingsprosess_schema_unit} IDENTIFIED BY ${felles_behandlingsprosess_schema_unit}');
+    EXECUTE IMMEDIATE ('CREATE USER DOLLAR{felles_behandlingsprosess_schema_unit} IDENTIFIED BY DOLLAR{felles_behandlingsprosess_schema_unit}');
   END IF;
 END;
 /
 
-GRANT CONNECT, RESOURCE, CREATE JOB, CREATE TABLE, CREATE SYNONYM, CREATE VIEW, CREATE MATERIALIZED VIEW TO ${felles_behandlingsprosess_schema_unit};
+GRANT CONNECT, RESOURCE, CREATE JOB, CREATE TABLE, CREATE SYNONYM, CREATE VIEW, CREATE MATERIALIZED VIEW TO DOLLAR{felles_behandlingsprosess_schema_unit};
 */
-DO $$
+/*DO $$
 DECLARE
   userexists integer;
 BEGIN
     SELECT COUNT(*)
     INTO userexists
     FROM pg_roles
-    WHERE rolname = lower('${felles_behandlingsprosess_schema_unit}');
+    WHERE rolname = lower('DOLLAR{felles_behandlingsprosess_schema_unit}');
     IF (userexists = 0)
     THEN
-      CREATE USER ${felles_behandlingsprosess_schema_unit} PASSWORD '${felles_behandlingsprosess_schema_unit}';
+      CREATE USER DOLLAR{felles_behandlingsprosess_schema_unit} PASSWORD 'DOLLAR{felles_behandlingsprosess_schema_unit}';
     END IF;
 END $$;
+*/
 
-/*CREATE DATABASE ${felles_behandlingsprosess_schema_unit};
+/*
 
-GRANT ALL PRIVILEGES ON DATABASE ${felles_behandlingsprosess_schema_unit} TO ${felles_behandlingsprosess_schema_unit};
+--Denne må kjøres først:
+
+CREATE DATABASE felles_behandlingsprosess_unit;
+
+CREATE USER felles_behandlingsprosess_unit PASSWORD 'felles_behandlingsprosess_unit';
+
+GRANT ALL PRIVILEGES ON DATABASE felles_behandlingsprosess_unit TO felles_behandlingsprosess_unit;
 */
 
