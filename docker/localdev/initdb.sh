3cdc00cc79fd052e43118194ac283cab5563e229
royag-nav <roy.andre.gundersen@nav.no>
2018-12-18 12:28:22 +0100

    opprett vl_fordeling_unit brukern i postgres docker

--- docker/localdev/initdb.sh
+++ docker/localdev/initdb.sh
@@ -19,3 +19,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	GRANT ALL PRIVILEGES ON DATABASE fpsak TO fpsak;
 EOSQL
 
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE vl_fordeling_unit;
+	CREATE USER vl_fordeling_unit PASSWORD '${POSTGRES_PASSWORD_VL_FORDELING_UNIT:-vl_fordeling_unit}';
+	GRANT ALL PRIVILEGES ON DATABASE vl_fordeling_unit TO vl_fordeling_unit;
+EOSQL
