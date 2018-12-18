25461b4bcdbe50112104f3448d7813fbf98c28cc
royag-nav <roy.andre.gundersen@nav.no>
2018-12-18 12:42:20 +0100

    fpfordel bruker i postgres docker

--- docker/localdev/initdb.sh
+++ docker/localdev/initdb.sh
@@ -24,3 +24,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	CREATE USER vl_fordeling_unit PASSWORD '${POSTGRES_PASSWORD_VL_FORDELING_UNIT:-vl_fordeling_unit}';
 	GRANT ALL PRIVILEGES ON DATABASE vl_fordeling_unit TO vl_fordeling_unit;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE fpfordel;
+	CREATE USER fpfordel PASSWORD '${POSTGRES_PASSWORD_FPFORDEL:-fpfordel}';
+	GRANT ALL PRIVILEGES ON DATABASE fpfordel TO fpfordel;
+EOSQL
