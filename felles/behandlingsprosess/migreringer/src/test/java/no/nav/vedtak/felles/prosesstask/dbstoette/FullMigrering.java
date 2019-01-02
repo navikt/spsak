aaf827ad3feb8a61671469b6f18983a215523dec
royag-nav <roy.andre.gundersen@nav.no>
2019-01-02 08:28:19 +0100

    bruk postgres 11 (for docker/localdev) + re-rename MigreringsTest slik at den ikke kjører automatisk (den kjører db.clean og Flyway 5 støtter ikke clean på Postgres 11)

--- felles/behandlingsprosess/migreringer/src/test/java/no/nav/vedtak/felles/prosesstask/dbstoette/FullMigrering.java
+++ felles/behandlingsprosess/migreringer/src/test/java/no/nav/vedtak/felles/prosesstask/dbstoette/FullMigrering.java
@@ -11,7 +11,7 @@ import no.nav.vedtak.felles.lokal.dbstoette.DatabaseStøtte;
  * Kjøres kun manuelt (aldri automatisk) siden den ikke heter xxTest.
  * Convenience klasse som kan kjøres dersom database er ute av synk med migreringer
  */
-public class MigreringTest {
+public class FullMigrering {
 
     @Test
     public void skal_utføre_full_migrering() throws Exception {
