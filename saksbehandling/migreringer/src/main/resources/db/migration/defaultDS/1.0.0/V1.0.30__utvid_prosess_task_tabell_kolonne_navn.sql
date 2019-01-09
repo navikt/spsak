251d2dac0c075df5038a41c6dc81931de2c25aa4
Carlsen, Frode Odde <Frode.Odde.Carlsen@nav.no>
2019-01-09 13:05:55 +0100

    Fix alter table column command

--- saksbehandling/migreringer/src/main/resources/db/migration/defaultDS/1.0.0/V1.0.30__utvid_prosess_task_tabell_kolonne_navn.sql
+++ saksbehandling/migreringer/src/main/resources/db/migration/defaultDS/1.0.0/V1.0.30__utvid_prosess_task_tabell_kolonne_navn.sql
@@ -1,11 +1,11 @@
-alter table prosess_task alter column siste_kjoering_server varchar(1000);
-alter table prosess_task alter column OPPRETTET_AV varchar(1000);
+alter table prosess_task alter column siste_kjoering_server TYPE varchar(1000);
+alter table prosess_task alter column OPPRETTET_AV TYPE varchar(1000);
 
-alter table PROSESS_TASK_FEILHAND alter column OPPRETTET_AV varchar(1000);
-alter table PROSESS_TASK_FEILHAND alter column ENDRET_AV varchar(1000);
-alter table PROSESS_TASK_FEILHAND alter column NAVN varchar(1000);
-alter table PROSESS_TASK_FEILHAND alter column KODE varchar(50);
+alter table PROSESS_TASK_FEILHAND alter column OPPRETTET_AV TYPE varchar(1000);
+alter table PROSESS_TASK_FEILHAND alter column ENDRET_AV TYPE varchar(1000);
+alter table PROSESS_TASK_FEILHAND alter column NAVN TYPE varchar(1000);
+alter table PROSESS_TASK_FEILHAND alter column KODE TYPE varchar(50);
 
-alter table PROSESS_TASK_TYPE alter column NAVN varchar(1000);
-alter table PROSESS_TASK_TYPE alter column OPPRETTET_AV varchar(1000);
-alter table PROSESS_TASK_TYPE alter column ENDRET_AV varchar(1000);
+alter table PROSESS_TASK_TYPE alter column NAVN TYPE varchar(1000);
+alter table PROSESS_TASK_TYPE alter column OPPRETTET_AV TYPE varchar(1000);
+alter table PROSESS_TASK_TYPE alter column ENDRET_AV TYPE varchar(1000);
