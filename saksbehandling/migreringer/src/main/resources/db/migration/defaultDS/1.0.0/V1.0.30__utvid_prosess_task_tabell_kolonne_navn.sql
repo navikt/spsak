861857cdcef33d00945da8bab8065c3cd99f3ae4
Carlsen, Frode Odde <Frode.Odde.Carlsen@nav.no>
2019-01-09 13:19:16 +0100

    Fiks feilhandtering_algortime lengde

--- saksbehandling/migreringer/src/main/resources/db/migration/defaultDS/1.0.0/V1.0.30__utvid_prosess_task_tabell_kolonne_navn.sql
+++ saksbehandling/migreringer/src/main/resources/db/migration/defaultDS/1.0.0/V1.0.30__utvid_prosess_task_tabell_kolonne_navn.sql
@@ -9,3 +9,5 @@ alter table PROSESS_TASK_FEILHAND alter column KODE TYPE varchar(50);
 alter table PROSESS_TASK_TYPE alter column NAVN TYPE varchar(1000);
 alter table PROSESS_TASK_TYPE alter column OPPRETTET_AV TYPE varchar(1000);
 alter table PROSESS_TASK_TYPE alter column ENDRET_AV TYPE varchar(1000);
+
+alter table PROSESS_TASK_TYPE alter column FEILHANDTERING_ALGORITME TYPE varchar(50);
