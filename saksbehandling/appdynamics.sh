8c261f5012fc2d3c984d23fafccdf14cdae1f182
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-01-10 09:29:38 +0100

    Simplify startup of app, use defaults from baseimage

--- saksbehandling/appdynamics.sh
+++ saksbehandling/appdynamics.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ -z ${APPDYNAMICS+x} ]; then
-    JAVA_OPTS="${JAVA_OPTS} -javaagent:/app/appdynamics/javaagent.jar "
+if test -f "/opt/appdynamics/javaagent.jar"; then
+    JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/appdynamics/javaagent.jar "
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=fpanalytics_${FASIT_ENVIRONMENT_NAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
