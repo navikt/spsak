4931f13452f6b098db42bf21d9e01ebbf9497705
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-01-14 10:02:05 +0100

    Revert "Remove klient and webapp dir"
    This reverts commit d47e6a1fa22e734298779f7ecde274d8a69dbc80.

--- saksbehandling/web/webapp/src/main/java/no/nav/foreldrepenger/web/server/jetty/JettyServer.java
+++ saksbehandling/web/webapp/src/main/java/no/nav/foreldrepenger/web/server/jetty/JettyServer.java
@@ -1,17 +1,18 @@
 package no.nav.foreldrepenger.web.server.jetty;
 
-import no.nav.foreldrepenger.web.app.ApplicationConfig;
-import no.nav.vedtak.isso.IssoApplication;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.eclipse.jetty.plus.jndi.EnvEntry;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.util.resource.ResourceCollection;
 import org.eclipse.jetty.webapp.MetaData;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import no.nav.foreldrepenger.web.app.ApplicationConfig;
+import no.nav.vedtak.isso.IssoApplication;
 
 public class JettyServer extends AbstractJettyServer {
 
@@ -98,7 +99,7 @@ public class JettyServer extends AbstractJettyServer {
     protected void migrerDatabaser() throws IOException {
         new DatabaseScript(dataSourceKonfig.getMigrationDatasource(), dataSourceKonfig.getMigrationScripts()).migrate();
     }
-
+    
     @Override
     protected WebAppContext createContext(AppKonfigurasjon appKonfigurasjon) throws IOException {
         WebAppContext webAppContext = super.createContext(appKonfigurasjon);
@@ -110,10 +111,17 @@ public class JettyServer extends AbstractJettyServer {
     private void updateMetaData(MetaData metaData) {
         // Find path to class-files while starting jetty from development environment.
         List<Class<?>> appClasses = Arrays.asList((Class<?>)ApplicationConfig.class, (Class<?>)IssoApplication.class);
-
+        
         List<Resource> resources = appClasses.stream().map(c -> Resource.newResource(c.getProtectionDomain().getCodeSource().getLocation())).collect(Collectors.toList());
-
+        
         metaData.setWebInfClassesDirs(resources);
+    }
+
+    @Override
+    protected ResourceCollection createResourceCollection() throws IOException {
+        return new ResourceCollection(
+            Resource.newResource(System.getProperty("klient", "./klient")), /* klient flyttes ut, så kan slettes etter hvert.*/
+            Resource.newClassPathResource("/web"));
     }
 
 }
